97a7453f840fa16d4d9ce089623fa010acb01780
Alex Davletov <aleksei-davletov@mail.ru>
2025-04-08 04:57:05 +0300

    Initial commit

---
+++ src/test/java/com/telros/demotelros/DemotelrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telros.demotelros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotelrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
